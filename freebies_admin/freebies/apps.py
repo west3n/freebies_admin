fd31a9f2adab83adef0133336270c03f3e130376
west3n <mirosshnikov@gmail.com>
2023-06-27 22:53:46 +0400

    new models

--- freebies_admin/freebies/apps.py
+++ freebies_admin/freebies/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FreebiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'freebies'
+
+    def ready(self):
+        from freebies import signals
